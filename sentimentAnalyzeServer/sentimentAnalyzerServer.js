46ae2844e2116ed6767443cae0f74e88504bf8b7
Ramesh Sannareddy <rsannareddy@gmail.com>
2022-03-21 16:31:23 +0000

    Fininshed Part A, B and C

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -22,16 +22,27 @@ function getNLUInstance() {
     /*Type the code to create the NLU instance and return it.
     You can refer to the image in the instructions document
     to do the same.*/
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2021-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: api_key
+        }),
+        serviceUrl: api_url
+    });
+    return naturalLanguageUnderstanding;
 }
 
 
 //The default endpoint for the webserver
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render('index.html');
-  });
+});
 
 //The endpoint for the webserver ending with /url/emotion
-app.get("/url/emotion", (req,res) => {
+app.get("/url/emotion", (req, res) => {
     // //Extract the url passed from the client through the request object
     // let urlToAnalyze = req.query.url
     // const analyzeParams = 
@@ -44,9 +55,9 @@ app.get("/url/emotion", (req,res) => {
     //                         }
     //         }
     //     }
-     
+
     //  const naturalLanguageUnderstanding = getNLUInstance();
-     
+
     //  naturalLanguageUnderstanding.analyze(analyzeParams)
     //  .then(analysisResults => {
     //     //Please refer to the image to see the order of retrieval
@@ -58,17 +69,17 @@ app.get("/url/emotion", (req,res) => {
 });
 
 //The endpoint for the webserver ending with /url/sentiment
-app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+app.get("/url/sentiment", (req, res) => {
+    return res.send("url sentiment for " + req.query.url);
 });
 
 //The endpoint for the webserver ending with /text/emotion
-app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+app.get("/text/emotion", (req, res) => {
+    return res.send({ "happy": "10", "sad": "90" });
 });
 
-app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+app.get("/text/sentiment", (req, res) => {
+    return res.send("text sentiment for " + req.query.text);
 });
 
 let server = app.listen(8080, () => {
